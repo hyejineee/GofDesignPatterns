d14b752bde4fb4cecea48e67f7bc74968df118d1
hyeonhyejin <dev.hyejin04@gmail.com>
2020-05-09 21:40:51 +0900

    사이즈 구하기 테스트 추가 & 기능 구현

--- Composite/src/main/kotlin/Composite/Entry.kt
+++ Composite/src/main/kotlin/Composite/Entry.kt
@@ -4,5 +4,5 @@ abstract class Entry(val name: String, size: Int) {
     open var size = size
     abstract fun add(entry: Entry)
     abstract fun printList(parentsDir: String = "")
-    abstract fun printSize(dir: String = "", size: Int = 0)
+    abstract fun printSize(dir: String = "")
 }
