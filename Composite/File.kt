066b965f753cc6f3b9e1a7dc6a4a699cf9236ef2
hyeonhyejin <dev.hyejin04@gmail.com>
2020-05-12 13:25:11 +0900

    Clean up the folder structure

---
+++ Composite/File.kt
@@ -0,0 +1,14 @@
+package Composite
+
+class File(name: String, size: Int = 0) : Entry(name, size) {
+    override fun add(entry: Entry) {
+    }
+
+    override fun printList(parentsDir: String) {
+        print("$parentsDir/$name")
+    }
+
+    override fun printSize(dir: String) {
+        print("$dir/$name , size = ${this.size}\n")
+    }
+}
