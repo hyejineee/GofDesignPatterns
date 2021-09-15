0ab4116c09ab027f945e8254ac50a57cd7498149
hyejineee <dev.hyejin04@gmail.com>
2021-09-15 20:03:58 +0900

    ㅇㅓㄷㅐㅂㅌㅓ ㅍㅐㅌㅓㄴ main ㅅㅜㅈㅓㅇ

--- src/com/hyejineee/design/adapter/Main.java
+++ src/com/hyejineee/design/adapter/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
         plugIn(h);
 
-        Adapter adapter = new Adapter(v);
+        Electronic110v adapter = new Adapter(v);
         plugIn(adapter);
 
 //        콘센트에 꽂을 수 있는 v가 다르기 때문에 에러 발생 -> 어댑터가 필요한 시점
